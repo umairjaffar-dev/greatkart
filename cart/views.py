2df0731cc6aa61bf7a909f52b6156f0c5a4c81f1
Umair Jaffar <umairjaffar.dev@gmail.com>
2025-12-10 10:08:07 +0500

    variations missing issue fixed!

--- cart/views.py
+++ cart/views.py
@@ -30,35 +30,34 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET.get('color')
-    size = request.GET.get('size')
-    
-    return HttpResponse(color + " " + size)
-    
-    
-    
-    ##  - Get the product from database.
-    try:
-        product = Product.objects.get(id=product_id)
-    except Product.DoesNotExist:
-        return redirect("shop")
+    color = request.GET.get("color")
+    size = request.GET.get("size")
 
-    ##  - Get cart if cart exists in database with same session id that make requests.
-    #   - else create a cart with unique car_id (cari_id=request.session.session_key)
-    try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-    except Cart.DoesNotExist:
-        cart = Cart.objects.create(cart_id=_cart_id(request))
-    cart.save()
+    if color and size:
+        return HttpResponse(color + " " + size)
+    else:
+        ##  - Get the product from database.
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return redirect("shop")
 
-    ## Get or create CartItem from database.
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
-        cart_item.save()
+        ##  - Get cart if cart exists in database with same session id that make requests.
+        #   - else create a cart with unique car_id (cari_id=request.session.session_key)
+        try:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create(cart_id=_cart_id(request))
+        cart.save()
+
+        ## Get or create CartItem from database.
+        try:
+            cart_item = CartItem.objects.get(product=product, cart=cart)
+            cart_item.quantity += 1
+            cart_item.save()
+        except CartItem.DoesNotExist:
+            cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+            cart_item.save()
 
     return redirect("cart")
 
--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-from .models import Product
+from .models import Product, variation_category_choices
 from category.models import Category
 
 from cart.models import CartItem
@@ -10,62 +10,80 @@ from cart.views import _cart_id
 
 
 # Create your views here.
-def store(request, category_slug = None):
+def store(request, category_slug=None):
     categories = None
     products = None
-    
+
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products   = Product.objects.filter(category=categories, is_available=True)
-        paginator = Paginator(products, 2) ## Get only 2 product in one page.
+        products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 2)  ## Get only 2 product in one page.
         page_number = request.GET.get("page")
         page_products = paginator.get_page(page_number)
         products_count = products.count()
-        
+
     else:
-        products = Product.objects.all().filter(is_available=True).order_by('id')
-        paginator = Paginator(products, 3) ## Get only 3 product in one page.
+        products = Product.objects.all().filter(is_available=True).order_by("id")
+        paginator = Paginator(products, 3)  ## Get only 3 product in one page.
         page_number = request.GET.get("page")
         page_products = paginator.get_page(page_number)
         products_count = products.count()
 
     context = {
-        'products':page_products,
-        'products_count':products_count,
+        "products": page_products,
+        "products_count": products_count,
     }
 
-    return render(request, 'store.html', context)
+    return render(request, "store.html", context)
 
 
 def product_details(request, category_slug, product_slug):
-    
+
     try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        in_cart        = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
-        
+        single_product = Product.objects.get(
+            category__slug=category_slug, slug=product_slug
+        )
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product
+        ).exists()
+
+        colors = single_product.variation_set.filter(
+            variation_category="color", is_active=True
+        )
+        sizes = single_product.variation_set.filter(
+            variation_category="size", is_active=True
+        )
+
+        print("--------------------------------------- COLOR:", colors)
+        print("--------------------------------------- SIZES:", sizes)
+
     except Exception as e:
         raise e
-    
+
     context = {
-        'single_product':single_product,
-        'in_cart':in_cart,
+        "single_product": single_product,
+        "in_cart": in_cart,
+        "colors": colors,
+        "sizes": sizes,
     }
-    
-    return render(request, 'product_details.html', context)
+
+    return render(request, "product_details.html", context)
 
 
 def search(request):
-    if 'keyword' in request.GET:
-        keyword = request.GET['keyword']
-        
+    if "keyword" in request.GET:
+        keyword = request.GET["keyword"]
+
         products = Product.objects.all()
-        
+
         if keyword:
-            products = products.filter(Q(product_name__icontains=keyword) | Q(description__icontains=keyword)).order_by("-created_at")
-    
+            products = products.filter(
+                Q(product_name__icontains=keyword) | Q(description__icontains=keyword)
+            ).order_by("-created_at")
+
     context = {
-        'products': products,
-        'products_count':products.count(),
+        "products": products,
+        "products_count": products.count(),
     }
-    
-    return render(request, 'store.html', context)
+
+    return render(request, "store.html", context)
